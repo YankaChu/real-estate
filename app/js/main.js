278474072f33eacbc59ecf4ff67f31f991af3050
YankaChu <tschyana@gmail.com>
2021-10-16 13:01:12 +0300

    added header, slider, search, popular

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
-});	
+  $('.slider__items').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/arrow-right.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+  });
+});
