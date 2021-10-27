8f2b8ef8d3aa7f7f947589c351ee5b06df8ff226
YankaChu <tschyana@gmail.com>
2021-10-27 23:32:45 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,6 @@ $(function(){
   $('.slider__items, .news__slider-inner').slick({
     prevArrow: '<button class="slick-btn slick-prev"></button>',
     nextArrow: '<button class="slick-btn slick-next"></button>',
-    infinite: false
+    infinite: false,
   });
 });
